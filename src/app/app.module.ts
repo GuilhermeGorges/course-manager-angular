24af506ec94fa05e76298dd5343cb4d347d43ff4
GuilhermeGorges <guilhermegabriel1992@gmail.com>
2021-09-03 19:47:04 -0300

    implements courses filter

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { CourseListComponent } from './courses/course-list.components';
+import { CourseListComponent } from './courses/course-list.component';
 
 @NgModule({
   declarations: [
