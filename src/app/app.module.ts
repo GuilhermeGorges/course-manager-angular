c970a266c15782604de5e6b97c7aafcdec56c8ea
guilhermegabriel <guilhermegabriel>
2021-09-04 21:25:56 -0300

    segregated error-404

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { CourseModule } from './courses/course.module';
-import { NavBarComponent } from './core/component/nav-bar/nav-bar.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 
-
 import { HttpClientModule } from '@angular/common/http';
-
-import { Error404Component } from './error-404/error-404.component';
 import { CoreModule } from './core/component/core.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Error404Component
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -25,9 +20,6 @@ import { CoreModule } from './core/component/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],
